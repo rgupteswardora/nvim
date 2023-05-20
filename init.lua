b3f90c25ca936ff3870fdd24fffc3ae0882ee2f2
R gupteswar Dora <96424349+rgupteswardora@users.noreply.github.com>
2023-05-20 19:14:20 +0530

    first

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("gupteswar.plugins-setup")
+require("gupteswar.core.options")
+require("gupteswar.core.keymaps")
+require("gupteswar.core.colorscheme")
+require("gupteswar.plugins.comment")
+require("gupteswar.plugins.nvim-tree")
